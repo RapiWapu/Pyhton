7a55d605386a713a2a0a095ff3368e0105b74e1e
Raffi <raffiachmadi52@gmail.com>
2025-10-23 17:21:16 +0700

    Create variabel.py

---
+++ variabel.py
@@ -0,0 +1,8 @@
+// disini kita belajar variabel 
+// Variabel itu sebuah wadah untuk menampung nilai tergantung dari jenisnya contoh
+
+my_name = "raffi"; //String berguna untuk memasukkan nilai sebuah kalimat atau kata
+my_age = 16; // Integrer berguna untuk memasukkan nilai BILANGAN BULAT
+my_gpa = 98.6; // Float berguna untuk memasukkan nilai dengan BILANGAN desimal
+is_student = True; // Boolean menunjukkan sebuah bilangan itu TRUE/FALSE
+
