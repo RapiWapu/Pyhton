5b5ad6b4d4b1279fd2d068d8bf3cbf8cd38e6ca5
Raffi <raffiachmadi52@gmail.com>
2025-10-23 18:22:48 +0700

    Update variabel.py

--- variabel.py
+++ variabel.py
@@ -1,8 +1,15 @@
 // disini kita belajar variabel 
 // Variabel itu sebuah wadah untuk menampung nilai tergantung dari jenisnya contoh
 
-my_name = "raffi"; //String berguna untuk memasukkan nilai sebuah kalimat atau kata
-my_age = 16; // Integrer berguna untuk memasukkan nilai BILANGAN BULAT
-my_gpa = 98.6; // Float berguna untuk memasukkan nilai dengan BILANGAN desimal
-is_student = True; // Boolean menunjukkan sebuah bilangan itu TRUE/FALSE
+my_name = "raffi" //String berguna untuk memasukkan nilai sebuah kalimat atau kata
+my_age = 16 // Integrer berguna untuk memasukkan nilai BILANGAN BULAT
+my_gpa = 98.6 // Float berguna untuk memasukkan nilai dengan BILANGAN desimal
+is_student = True // Boolean menunjukkan sebuah bilangan itu TRUE/FALSE
+          
+// keempat variabel itu semua akan berguna kelak
 
+// Fungsi dari print adalah untuk mengeluarkan sebuah variabel ataupun kata ke output 
+print(f"Your name is {my_name}")
+print(f"Your age is {my_age}")
+print(f"Your GPA is {my_gpa}")
+print(f"Are you student ? {is_student}")
