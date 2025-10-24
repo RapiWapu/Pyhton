46842adaa03af153ff71a313e1851482adde6ab0
Raffi <raffiachmadi52@gmail.com>
2025-10-24 20:17:05 +0700

    Update typecasting.py

--- typecasting.py
+++ typecasting.py
@@ -8,12 +8,20 @@ iam_student = True
 
 // jadi diatas ada beberapa variabel bisa kita ganti sesuka hati
 
+my_name = bool(my_name)
 my_age = str(my_age)
 my_gpa = int(my_gpa)
 
 // kita bisa mnggunakan perintah type untuk mengetahui tipe variabel yang telah di ubah tersebut
+
 print(type(my_age))
 print(my_age)
 
 print(type(my_gpa))
 print(my_gpa)
+
+// jika di dalam variabel my_name ada isinya yaitu "Raffi" maka hasil di output adalah True karena ada isinya
+// sebaliknya jika di dalam variabel my_name kosong "" maka hasilnya akan False karena tidak ada isinya
+
+print(type(my_name))
+print(my_name)
