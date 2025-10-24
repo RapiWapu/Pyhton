7cc6e128f7d8f2e8d049d72451ea97bea91a4d40
Raffi <raffiachmadi52@gmail.com>
2025-10-24 20:08:44 +0700

    Create typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+// typecasting itu proses mengubah nilai dari sebuah tipe variabel ke tipe variabel yang lain
+// menggunakan str(), int(),float(),bool()
+
+my_name = "Raffi"
+my_age = 16
+my_gpa = 89.78
+iam_student = True
+
+// jadi diatas ada beberapa variabel bisa kita ganti sesuka hati
+
+my_age = str(my_age)
+my_gpa = int(my_gpa)
+
+// kita bisa mnggunakan perintah type untuk mengetahui tipe variabel yang telah di ubah tersebut
+print(type(my_age))
+print(my_age)
+
+print(type(my_gpa))
+print(my_gpa)
