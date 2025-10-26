e7d23dc21ef1c1678e859add59d6e21a8b2818c9
Raffi <raffiachmadi52@gmail.com>
2025-10-26 10:01:48 +0700

    Fix comments formatting in userinput.py

--- userinput.py
+++ userinput.py
@@ -1,5 +1,5 @@
-// kegunaan userinput adalah pengguna bisa mengisi data dari variabel tersebut 
-// default dari input ini adalah str() atau string(), jadi kita perlu casting jika ingin mengubahnya seperti variabel age
+#kegunaan userinput adalah pengguna bisa mengisi data dari variabel tersebut 
+#default dari input ini adalah str() atau string(), jadi kita perlu casting jika ingin mengubahnya seperti variabel age
 
 user = input("What is your name?: ")
 age = int(input("What is your age?: "))
