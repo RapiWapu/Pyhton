cdaca0ba878e58c32b9c09a0fdc896435d2f0a98
Raffi <raffiachmadi52@gmail.com>
2025-10-26 22:05:41 +0700

    Implement basic arithmetic operations in aritmetic.py
    Added basic arithmetic operations and comments for clarity.

---
+++ aritmetic.py
@@ -0,0 +1,41 @@
+import math #agar bisa memasukkan simbol di aritmatika seperti phi
+
+friends = 0
+
+#Disini ada 4 perhitungan dasar matematika yaitu +,-,* dan /
+friends = friends + 1
+friends += 1 #statement += sama aja kaya yang atas, ini untuk mempersingkat aja
+friends = friends - 2
+friends -= 2
+friends = friends * 3
+friends *= 3
+friends = friends / 2
+friends /= 2
+
+friends = friends ** 2 #bilangan eksponen atau berpangkat
+friends **= 2
+remainder = friends % 3 #modulus sisa dari hasil pembagian jika yang di bagi tidak pas 
+
+print(friends)
+
+#x = 3.14
+#y = 4
+#z = 5
+
+#result = round(x) pembulatan
+#result = abs(y) jarak bilangan variabel int atau float ke 0
+#result = pow(4, 3) bilangan eksponen 4³ 4x4x4 (Basis, ekponen)
+#result = max(x, y, z) mengidentifikasi nilai terbesar yang mana
+#result = min(x, y, z) mengidentifikasi nilai terkecil yang mana
+
+#print(result)
+
+#x = 9.1
+
+#print(math.pi) #bilangan phi
+#print(math.e) bilangan konstanta
+#print(math.sqrt(x)) #Fungsi akar kuadrat adalah suatu fungsi matematika yang menghasilkan akar kuadrat positif dari sebuah nilai masukan, biasanya ditulis sebagai f(x) = √x. Fungsi ini hanya menerima bilangan non-negatif (nol atau positif) sebagai masukan dan mengembalikan hasil akar kuadrat positif dari masukan tersebut.
+#result = math.ceil(x) #Ceiling function berguna untuk membulatkan bilangan float atau desimal ke atas misal 9.1 menjadi 10
+#result = math.floor(x) #Floor function berguna untuk membulatkan bilangan float atau desimal ke bawah misal 9.9 menjadi 9
+
+#print(result)
