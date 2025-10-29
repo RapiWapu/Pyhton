3d196196707e2e94c80dc357dd8b94d8bef4037d
Raffi <raffiachmadi52@gmail.com>
2025-10-29 17:18:23 +0700

    Add age-based conditional statements

--- ifstatement.py
+++ ifstatement.py
@@ -1,4 +1,13 @@
 #if berguna melakukan perintah jika kondisinya True
 #else berguna jika kondisi if adalah False maka menjadikan else True
 
+age = int(input("Enter your age: "))
 
+if age >= 100:
+    print("You are to old")
+elif age >= 18:
+    print("You signup!")
+elif age < 0:
+    print("You dont have age lol")
+else:
+    print("You can't make a credit card now")
